c22d225a82e807e47d77ad5d3133046c88cd99b9
mzargari <mz2113@gmail.com>
2022-08-12 11:37:28 -0700

    Initial commit

---
+++ R/kavod_kardan.R
@@ -0,0 +1,16 @@
+#' Title
+#'
+#' @param donation The amount you would like to donate for a torah portion
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+kavod_kardan <- function(donation) {
+  if (donation %% 13 == 0 | donation %% 18 == 0){
+    print("Yehad, Shetayim, Shalosh, Zakha")
+  } else {
+    print("Yadeh khoda bash.")
+  }
+}
